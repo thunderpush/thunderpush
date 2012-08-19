aaf384e46b1db37b424d58f4f651f811c3834491
Krzysysztof Jagiello <balonyo@gmail.com>
2012-08-19 12:08:23 +0200

    Removing misplaced file

--- thunderpush/service.py
+++ thunderpush/service.py
@@ -6,6 +6,9 @@ import tornado.web
 from tornado import websocket, web
 from tornado.ioloop import IOLoop
 from sockjs.tornado import SockJSRouter, SockJSConnection
+from thunderpush.api import UserCountHandler, ChannelMessageHandler
+from thunderpush.messenger import Messenger
+from thunderpush.sortingstation import SortingStation
 
 try:
     import json
@@ -14,136 +17,6 @@ except ImportError:
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
-
-class Messenger(object):
-    """ 
-    Handles dispatching messages to Channels and Users
-    for given client.
-    """
-    
-    def __init__(self, apikey, apisecret, *args, **kwargs):
-        self.apikey = apikey
-        self.apisecret = apisecret
-        self.users = {}
-        self.channels = {}
-        self.user_count = 0
-
-    def send_to_channel(self, channel, message):
-        """ 
-        Sends a message to given channel
-        Returns a count of messages sent
-        """
-
-        try:
-            channel = self.channels[channel]
-        except KeyError:
-            return 0
-
-        count = 0
-
-        for user in channel:
-            user.send(message)
-            count += 1
-
-        return count
-
-    def send_to_user(self, user, message):
-        pass
-
-    def subsribe_user(self, user):
-        self.user_count += 1
-
-    def subsribe_user_to_channel(self, user, channel):
-        logger.debug("%s subscribed to %s." % (user.userid, channel,))
-
-        self.channels.setdefault(channel, []).append(user)
-
-        logger.debug("User count in %s: %d." % (channel, self.get_channel_user_count(channel)))
-
-    def unsubscribe_user(self, user):
-        for name in self.channels.iterkeys():
-            try:
-                self.channels[name].remove(user)
-            except (KeyError, ValueError):
-                pass
-
-        self.user_count -= 1
-
-    def get_user_count(self):
-        return self.user_count
-
-    def get_channel_user_count(self, channel):
-        channel = self.channels.get(channel, None)
-
-        if channel:
-            return len(channel)
-        else:
-            logger.debug("Channel %s not found." % (channel,))
-            return 0
-
-class SortingStation(object):
-    """ Handles dispatching messages to Messengers. """
-
-    def __init__(self, *args, **kwargs):
-        self.messengers_by_apikey = {}
-        self.messengers_by_apisecret = {}
-
-    def create_messenger(self, apikey, apisecret):
-        messenger = Messenger(apikey, apisecret)
-
-        self.messengers_by_apikey[apikey] = messenger
-        self.messengers_by_apisecret[apisecret] = messenger
-
-    def delete_messenger(self, messenger):
-        del self.messengers_by_apikey[messenger.apikey]
-        del self.messengers_by_apisecret[messenger.apisecret]
-
-    def get_messenger_by_apikey(self, apikey):
-        try:
-            return self.messengers_by_apikey[apikey]
-        except KeyError:
-            return None
-
-    def get_messenger_by_apisecret(self, apisecret):
-        try:
-            return self.messengers_by_apisecret[apisecret]
-        except KeyError:
-            return None
-
-    def dispatch(self, apisecret, message, *args, **kwargs):
-        """ Sends a message to specified channel or user. """
-
-        messenger = self.get_messenger_by_apisecret(apisecret)
-        
-        if messenger:
-            if "channel" in kwargs:
-                messenger.send_to_channel(kwargs['channel'])
-            elif "user" in kwargs:
-                pass
-
-class ChannelMessageHandler(tornado.web.RequestHandler):
-    def post(self, *args, **kwargs):
-        messenger = ss.get_messenger_by_apisecret(kwargs['apisecret'])
-
-        if messenger:
-            count = messenger.send_to_channel(kwargs['channel'], self.request.body)
-
-            logger.debug("Message has been sent to %d users." % count)
-
-            self.write(json.dumps({"status": "ok", "count": count}) + "\n")
-        else:
-            self.write(json.dumps({"status": "error", "message": "Wrong API secret."}) + "\n")
-
-class UserCountHandler(tornado.web.RequestHandler):
-    def get(self, *args, **kwargs):
-        messenger = ss.get_messenger_by_apisecret(kwargs['apisecret'])
-
-        if messenger:
-            user_count = messenger.get_user_count()
-
-            self.write(json.dumps({"status": "ok", "count": user_count}) + "\n")
-        else:
-            self.write(json.dumps({"status": "error", "message": "Wrong API secret."}) + "\n")
 
 class ThunderSocketHandler(SockJSConnection):
     def on_open(self, info):
@@ -203,8 +76,8 @@ class ThunderSocketHandler(SockJSConnection):
 ThunderRouter = SockJSRouter(ThunderSocketHandler, "/connect")
 
 application = tornado.web.Application([
-    (r"/1\.0\.0/(?P<apisecret>.+)/users/", UserCountHandler),
-    (r"/1\.0\.0/(?P<apisecret>.+)/channels/(?P<channel>.+)/", ChannelMessageHandler),
+    (r"/1\.0\.0/(?P<apikey>.+)/users/", UserCountHandler),
+    (r"/1\.0\.0/(?P<apikey>.+)/channels/(?P<channel>.+)/", ChannelMessageHandler),
 ] + ThunderRouter.urls, debug=True)
 
 if __name__ == "__main__":
