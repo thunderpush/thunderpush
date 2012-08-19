4f1445a1f1a918867f5b3753bd0aab527cbdc277
Krzysysztof Jagiello <balonyo@gmail.com>
2012-08-19 17:37:03 +0200

    Removing dead code

--- thunderpush/sortingstation.py
+++ thunderpush/sortingstation.py
@@ -32,24 +32,4 @@ class SortingStation(object):
         del self.messengers_by_apisecret[messenger.apisecret]
 
     def get_messenger_by_apikey(self, apikey):
-        try:
-            return self.messengers_by_apikey[apikey]
-        except KeyError:
-            return None
-
-    def get_messenger_by_apisecret(self, apisecret):
-        try:
-            return self.messengers_by_apisecret[apisecret]
-        except KeyError:
-            return None
-
-    def dispatch(self, apisecret, message, *args, **kwargs):
-        """ Sends a message to specified channel or user. """
-
-        messenger = self.get_messenger_by_apisecret(apisecret)
-        
-        if messenger:
-            if "channel" in kwargs:
-                messenger.send_to_channel(kwargs['channel'])
-            elif "user" in kwargs:
-                pass
+        return self.messengers_by_apikey.get(apikey, None)
