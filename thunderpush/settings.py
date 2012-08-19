79fb87c29652ac5f05150f6b2de6c91ffc0a784b
Krzysysztof Jagiello <balonyo@gmail.com>
2012-08-19 21:41:00 +0200

    Default settings for the project

---
+++ thunderpush/settings.py
@@ -0,0 +1,16 @@
+# This file contains default settings for this project.
+# If you want to customize this settings, use command line arguments
+# to do so, instead of modifying this file.
+
+# Port to which the server will be bound.
+PORT = 8080
+
+# Host to which the server will be binded.
+HOST = ''
+
+# Debug mode? Tornado will restart the server whenever any project file 
+# change. Very useful for development.
+DEBUG = False
+
+# This sets the logging level to DEBUG.
+VERBOSE = False
